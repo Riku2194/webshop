b3889cbaa0d583879df5b6612385e7f59861541c
kazuya-0914 <kazu.work.0914@gmail.com>
2026-01-26 15:52:11 +0900

    1/26 コミット

--- products/admin.py
+++ products/admin.py
@@ -6,9 +6,8 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "price", "is_active", "categoriese", "created_at", "updated_at")
+    list_display = ("name", "price", "is_active", "created_at")
     search_fields = ("name", )
-    prepopulated_fields = {"slug": ("name",)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
